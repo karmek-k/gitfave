c3a004d05a2ae8ce608cc9a8dea18fdc0624f862
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-27 16:17:52 +0200

    Fix nunjucks not finding templates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import nunjucks from 'nunjucks';
 
@@ -5,13 +6,13 @@ const app = express();
 const port = process.env.PORT ?? 8000;
 
 // Templates
-nunjucks.configure('views', {
+nunjucks.configure(path.join('src', 'views'), {
   autoescape: true,
   express: app
 });
 
 app.get('/', (req, res) => {
-  return res.render('index');
+  return res.render('index.njk');
 });
 
 app.listen(port, () => console.log(`Listening at port ${port}`));
