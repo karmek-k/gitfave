13231ea851ccd3ed61ded7d9df7c4866bc2619bf
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-28 18:01:00 +0200

    Configure the db connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,12 @@
 import { ConnectionOptions } from 'typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const options: ConnectionOptions = {
-  type: 'postgres'
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [],
+  synchronize: true
 };
 
 export default options;
