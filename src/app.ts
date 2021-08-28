0706b03253181ea51384ca53e9c71790dab7057c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-28 18:37:39 +0200

    Use the strategy factory in passport

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import 'reflect-metadata';
 import path from 'path';
 import express from 'express';
 import nunjucks from 'nunjucks';
+import passport from 'passport';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import createAuthStrategy from './config/authStrategy';
 
 import MainRouter from './controllers/main';
 
@@ -17,6 +20,14 @@ nunjucks.configure(path.join('src', 'views'), {
 
 // Middleware
 app.use(express.static(path.join('src', 'assets')));
+app.use(passport.initialize());
+passport.use(
+  createAuthStrategy(
+    process.env.CLIENT_ID!,
+    process.env.CLIENT_SECRET!,
+    process.env.CALLBACK_URL!
+  )
+);
 
 // Routes
 app.use('/', MainRouter);
