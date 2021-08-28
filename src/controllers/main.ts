58723f5b1b845a220ef90fa48f1c7cb4d25eecc2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-28 19:05:19 +0200

    Check if the user is logged in

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.render('main/index.njk');
+  return res.render('main/index.njk', { loggedIn: req.isAuthenticated() });
 });
 
 export default router;
