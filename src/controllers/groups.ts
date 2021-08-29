ae106e2fd451a255d43885b0778ee7edbe9f030c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-29 16:32:52 +0200

    Create group creation action

--- src/controllers/groups.ts
+++ src/controllers/groups.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import User from '../models/User';
+import csrf from '../middleware/csrf';
 
 const router = Router();
 
@@ -7,6 +8,10 @@ router.get('/', async (req, res) => {
   const { groups } = req.user as User;
 
   return res.render('groups/all.njk', { groups });
+});
+
+router.get('/new', csrf, async (req, res) => {
+  return res.render('groups/new.njk', { csrfToken: req.csrfToken() });
 });
 
 export default router;
