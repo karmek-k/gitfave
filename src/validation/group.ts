384e8bf47338087280b78029ca4431a3f4f987e2
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-08-31 15:01:14 +0200

    Disable escaping group name as it breaks nunjucks

--- src/validation/group.ts
+++ src/validation/group.ts
@@ -1,3 +1,3 @@
 import { body } from 'express-validator';
 
-export default [body('name').isLength({ min: 3, max: 30 }).trim().escape()];
+export default [body('name').isLength({ min: 3, max: 30 }).trim()];
